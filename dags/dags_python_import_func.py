c7900bf4329752e27c1a744f0fcfe500c7e2dce6
yubeomcheon <a89428242@gmail.com>
2025-04-06 20:24:30 +0900

    공통 외부함수 수정 후 추가

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -2,7 +2,7 @@ from airflow import DAG
 import pendulum
 import datetime
 from airflow.operators.python import PythonOperator
-from plugins.common.common_func import get_sftp ## /plugins 까지 하위 폴더로 잡게되면 에러 발생
+from common.common_func import get_sftp ## /plugins 까지 하위 폴더로 잡게되면 에러 발생
 
 with DAG(
     dag_id = "dags_python_import_func",
