7f7bab541785039c15d7e4391bc866e6604622aa
yubeomcheon <a89428242@gmail.com>
2025-04-06 17:08:59 +0900

    커넥션 테스트용 dag 업로드

--- dags/dags_conn_test.py
+++ dags/dags_conn_test.py
@@ -6,7 +6,7 @@ from airflow.operators.empty import EmptyOperator
 with DAG(
     dag_id = "dags_conn_test",
     schedule = None,
-    start_date = pendulum.datetime(2025, 4, 6, tz = "Asia.Seoul"),
+    start_date = pendulum.datetime(2025, 4, 6, tz = "Asia/Seoul"),
     catchup = False
 ) as dag:
     
