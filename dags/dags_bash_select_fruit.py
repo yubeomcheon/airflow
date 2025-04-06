979c1748424cb683ecbaa32710e5f6f59e5a3008
yubeomcheon <a89428242@gmail.com>
2025-04-06 18:13:23 +0900

    select_fruit.sh 수행하는 dag 업로드

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -12,12 +12,12 @@ with DAG(
     
     t1_orange = BashOperator(
         task_id = "t1_orange",
-        bash_command = "opt/airflow/plugins/shell/select_fruit.sh ORANGE",
+        bash_command = "/opt/airflow/plugins/shell/select_fruit.sh ORANGE",
     )
     
     t2_avocado = BashOperator(
         task_id = "t2_avocado",
-        bash_command = "opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
+        bash_command = "/opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
     )
     
     t1_orange >> t2_avocado
